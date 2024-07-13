b9288e48aa5ec78d7e51686bd3d0be542bd068a3
mishanya <homakhaker@gmail.com>
2024-07-13 13:42:50 +0200

    Added user query

--- src/main/kotlin/com/lifehon_front/controllers/HomeController.kt
+++ src/main/kotlin/com/lifehon_front/controllers/HomeController.kt
@@ -1,6 +1,7 @@
 package com.lifehon_front.controllers
 
 import com.HobbiesQuery
+import com.UsersQuery
 import com.lifehon_front.service.ApolloServerConnector
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -12,9 +13,11 @@ class HomeController(val apolloServerConnector: ApolloServerConnector) {
 
     @GetMapping("/")
     suspend fun home(model: Model): String {
-        val response = apolloServerConnector.apolloClient.query(HobbiesQuery()).execute();
+        val hobbiesResponse = apolloServerConnector.apolloClient.query(HobbiesQuery()).execute();
+        val usersResponse = apolloServerConnector.apolloClient.query(UsersQuery()).execute();
 
-        model["hobbies"] = response.data?.hobbies
+        model["hobbies"] = hobbiesResponse.data?.hobbies
+        model["users"] = usersResponse.data?.users
         return "home"
     }
 }
