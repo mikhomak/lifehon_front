7e536b196ff1dc6853efd72ed19f27746b08d98a
mishanya <homakhaker@gmail.com>
2024-07-19 21:00:03 +0200

    Added hobby adding to the user

--- src/main/kotlin/com/lifehon_front/controllers/MyAccountController.kt
+++ src/main/kotlin/com/lifehon_front/controllers/MyAccountController.kt
@@ -1,6 +1,7 @@
 package com.lifehon_front.controllers
 
-import com.UserForNameQuery
+import com.AddHobbyToUserMutation
+import com.GetUserForNameQuery
 import com.lifehon_front.logger
 import com.lifehon_front.service.ApolloServerConnector
 import com.lifehon_front.service.LifehonUtils
@@ -8,17 +9,43 @@ import jakarta.servlet.http.HttpServletRequest
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.*
 
 @Controller
+@RequestMapping("/account/")
 class MyAccountController(val apolloServerConnector: ApolloServerConnector) {
 
-    @GetMapping("/account/{name}")
+    @GetMapping("/{name}")
     suspend fun home(@PathVariable(name = "name") name: String, request: HttpServletRequest, model: Model): String {
-        val userResponse = apolloServerConnector.apolloClient.query(UserForNameQuery(name, 0)).execute();
+        val userResponse = apolloServerConnector.apolloClient.query(GetUserForNameQuery(name, 0)).execute();
         model["user"] = userResponse.data?.getUserForName;
+        model["is_the_same_account"] =
+            userResponse.data?.getUserForName?.name?.equals(LifehonUtils.getSessionUser(request));
         return "my-account"
+    }
+
+    @PostMapping("/addHobby")
+    suspend fun addHobby(
+        @RequestParam(name = "hobbyName") hobbyName: String,
+        request: HttpServletRequest,
+        model: Model
+    ): String {
+        LifehonUtils.getSessionUser(request)?.name?.let {
+            val addHobbyToUserResponse =
+                apolloServerConnector.apolloClient.mutation(AddHobbyToUserMutation(it, hobbyName)).execute();
+            if (addHobbyToUserResponse.hasErrors()) {
+                logger.error {
+                    "Error at adding the hobby [$hobbyName] to user [$it] and the error is [${
+                        addHobbyToUserResponse.errors?.map { toString() }?.joinToString { "," }
+                    }]"
+                };
+                LifehonUtils.addErrorToModel("Error at adding the hobby", model);
+            } else {
+                logger.info { "Added hobby [$hobbyName] to user [$it]" };
+            }
+            return "redirect:/account/${it}";
+        }
+        return "redirect:/";
     }
 
 }
--- src/main/kotlin/com/lifehon_front/service/LifehonUtils.kt
+++ src/main/kotlin/com/lifehon_front/service/LifehonUtils.kt
@@ -2,12 +2,24 @@ package com.lifehon_front.service
 
 import com.CheckTokenQuery
 import jakarta.servlet.http.HttpServletRequest
+import org.springframework.ui.Model
 
 class LifehonUtils {
     companion object {
         fun getSessionUser(request: HttpServletRequest): CheckTokenQuery.CheckToken? {
             return request.session.getAttribute("current_user") as CheckTokenQuery.CheckToken?;
+        }
 
+        fun addErrorToModel(error: String, model: Model) {
+            var errorsAttribute: MutableList<String>? = model.getAttribute("errors") as? MutableList<String>;
+
+            if (errorsAttribute == null) {
+                errorsAttribute = mutableListOf(error);
+            } else {
+                errorsAttribute.add(error);
+            }
+
+            model.addAttribute(errorsAttribute)
         }
     }
 }
